d9b71e82a1f6334bebbe776ca9471496c2cb0403
georgegelitashvili <george.gelitashvili@gmail.com>
2023-05-01 21:39:04 +0400

    update

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
-import { DevSettings } from "react-native";
+import { DevSettings, Alert } from "react-native";
 import NetInfo from "@react-native-community/netinfo";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
-import { Alert } from "react-native";
 import "react-native-gesture-handler";
 
 import { store } from "./src/redux/Store";
-
 import Main from "./src/Main";
 
 export default function App() {
