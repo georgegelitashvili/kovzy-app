5cab201c715aab96ff98d5050558f747b26e2312
georgegelitashvili <george.gelitashvili@gmail.com>
2023-07-17 13:03:27 +0400

    update

--- src/components/modal/OrdersModalAccept.js
+++ src/components/modal/OrdersModalAccept.js
@@ -25,10 +25,9 @@ export default function OrdersModalContent(props) {
 
   const [orderData, setOrderData] = useState({});
   const [deliveron, setDeliveron] = useState({ data: props.items, error: "" });
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(props.items[0].value || null);
   const [loading, setLoading] = useState(false);
   const { dictionary } = useContext(LanguageContext);
-
 
   const acceptOrder = () => {
     if(deliveron?.data != null) {
@@ -47,6 +46,8 @@ export default function OrdersModalContent(props) {
             {text: 'OK', onPress: () => props.hideModal()},
           ]);
         }
+      }).catch((error) => {
+        console.log(error);
       });
     }
   };
