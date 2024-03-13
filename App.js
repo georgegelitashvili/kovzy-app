9af65e81976ddb395f8f3a7043544a5c4481805b
georgegelitashvili <george.gelitashvili@gmail.com>
2024-03-13 19:49:33 +0400

    update

--- App.js
+++ App.js
@@ -7,8 +7,15 @@ import NetInfo from "@react-native-community/netinfo";
 import "react-native-gesture-handler";
 
 import Main from "./src/Main";
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://2ae499816c71fe2e649d2d50a9fe6f9c@o4506904411439104.ingest.us.sentry.io/4506904415764480',
+  enableInExpoDevelopment: true, // Enable Sentry in Expo development mode
+  debug: true, // Enable debug mode
+});
+
+function App() {
   const [isConnected, setIsConnected] = useState(true);
 
   useEffect(() => {
@@ -19,33 +26,33 @@ export default function App() {
     return () => {removeSubscription()};
   }, []);
 
-  // useEffect(() => {
-  //   async function checkForUpdates() {
-  //     try {
-  //       const { isAvailable } = await Updates.checkForUpdateAsync();
-  //       if (isAvailable) {
-  //         Alert.alert(
-  //           'Update Available',
-  //           'A new version of the app is available. Do you want to update?',
-  //           [
-  //             { text: 'Cancel', style: 'cancel' },
-  //             {
-  //               text: 'Update',
-  //               onPress: async () => {
-  //                 // Perform the update
-  //                 Linking.openURL("http://play.google.com/store/apps/details?id=com.kovzy.app")
-  //               },
-  //             },
-  //           ]
-  //         );
-  //       }
-  //     } catch (error) {
-  //       console.error('Error checking for updates:', error);
-  //     }
-  //   }
+  useEffect(() => {
+    async function checkForUpdates() {
+      try {
+        const { isAvailable } = await Updates.checkForUpdateAsync();
+        if (isAvailable) {
+          Alert.alert(
+            'Update Available',
+            'A new version of the app is available. Do you want to update?',
+            [
+              { text: 'Cancel', style: 'cancel' },
+              {
+                text: 'Update',
+                onPress: async () => {
+                  // Perform the update
+                  Linking.openURL("http://play.google.com/store/apps/details?id=com.kovzy.app")
+                },
+              },
+            ]
+          );
+        }
+      } catch (error) {
+        console.error('Error checking for updates:', error);
+      }
+    }
 
-  //   checkForUpdates();
-  // }, []);
+    checkForUpdates();
+  }, []);
 
 
   return (
@@ -60,6 +67,8 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
+
+export default Sentry.wrap(App);
 
 
 const styles = StyleSheet.create({
