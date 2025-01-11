11b6cc7136a6e795ebc6825d806b7f903ea6b773
bagrati <jgamadzebagrati@gmail.com>
2025-01-11 22:08:03 +0400

    remove postpone order things

--- src/QrOrders.js
+++ src/QrOrders.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { EnteredOrdersList } from "./components/qrorders/EnteredOrders";
-import { PostponeOrders } from "./components/qrorders/PostponeOrders";
 import { AcceptedOrdersList } from "./components/qrorders/AcceptedOrders";
 import { LanguageContext } from "./components/Language";
 
@@ -10,22 +8,6 @@ const Tab = createMaterialTopTabNavigator();
 
 export default function TabContent(props) {
   const { dictionary } = useContext(LanguageContext);
-  const [postponeOrderShow, setPostponeOrderShow] = useState(false);
-
-  useEffect(() => {
-    const loadStoredValue = async () => {
-      try {
-        const storedValue = await AsyncStorage.getItem('postponeOrderShow');
-        if (storedValue !== null) {
-          setPostponeOrderShow(JSON.parse(storedValue));
-        }
-      } catch (error) {
-        console.error('Failed to load stored value', error);
-      }
-    };
-
-    loadStoredValue();
-  }, []);
 
   return (
     <Tab.Navigator
@@ -40,12 +22,6 @@ export default function TabContent(props) {
         component={EnteredOrdersList}
         options={{ tabBarLabel: dictionary["nav.pendingOrders"], unmountOnBlur: true }}
       />
-
-      {postponeOrderShow ? (<Tab.Screen
-        name="PlannedOrders"
-        component={PostponeOrders}
-        options={{ tabBarLabel: dictionary["nav.plannedOrders"], unmountOnBlur: true }}
-        />): null}
 
       <Tab.Screen
         name="AcceptedOrders"
